fd7bea0da3129a1447c8210ae594bfc2c8ae5e1a
audrie talbot <audrie.talbot@gmail.com>
2015-04-28 14:57:50 -0700

    Take user input of place and saves it to a running list of places.

--- lib/places.rb
+++ lib/places.rb
@@ -1,4 +1,6 @@
 class Place
+  @@all_places = [] #Declares a class variable 'all_places' as an empty array
+
   define_method(:initialize) do |description| #Creates the new class 'Place' and passes in description as a parameter
     @description = description #Creates the instance variable 'description'
   end
@@ -7,7 +9,15 @@ class Place
     @description
   end
 
-  define_singleton_method(:all) do
-    []
+  define_singleton_method(:all) do #Creates class method .all which returns the contents of the class variable 'all_places'.
+    @@all_places
+  end
+
+  define_method(:save) do #Creates a method to take the user input and push it into the 'all_places' array.
+    @@all_places.push(self)
+  end
+
+  define_singleton_method(:clear) do #Creates a class method to clear all data from the 'all_tasks' array.
+    @@all_places = []
   end
 end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -1,15 +1,36 @@
 require('rspec')
-require('Places')
+require('places')
 
 describe(Place) do
+    before() do
+      Place.clear()
+    end
+
   describe("#description") do
     it("lets you give it a description") do
       test_place = Place.new("Georgia")  #Calls the initialize method
       expect(test_place.description()).to(eq("Georgia"))
     end
   end
+
   describe(".all") do
     it("is empty at first") do
+      expect(Place.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a place to the array of saved places") do
+      test_place = Place.new("Georgia")
+      test_place.save()
+      expect(Place.all()).to(eq([test_place]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved places") do
+      Place.new("Georgia").save()
+      Place.clear()
       expect(Place.all()).to(eq([]))
     end
   end
