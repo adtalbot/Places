22006beb56d6e8e3cf84a495befe034e702b6536
audrie talbot <audrie.talbot@gmail.com>
2015-04-28 14:12:12 -0700

    Use class method .all to store the user inputs.

--- lib/places.rb
+++ lib/places.rb
@@ -1,9 +1,13 @@
 class Place
-  define_method(:initialize) do |description|
-    @description = description
+  define_method(:initialize) do |description| #Creates the new class 'Place' and passes in description as a parameter
+    @description = description #Creates the instance variable 'description'
   end
 
-  define_method(:description) do
+  define_method(:description) do #Creates a method for instance variable 'description' which outputs the contents of the variable.
     @description
+  end
+
+  define_singleton_method(:all) do
+    []
   end
 end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -1,11 +1,16 @@
 require('rspec')
 require('Places')
 
-describe(Place) do  
+describe(Place) do
   describe("#description") do
     it("lets you give it a description") do
       test_place = Place.new("Georgia")  #Calls the initialize method
       expect(test_place.description()).to(eq("Georgia"))
+    end
+  end
+  describe(".all") do
+    it("is empty at first") do
+      expect(Place.all()).to(eq([]))
     end
   end
 end
